adc5d9a4264a3ee0005ae9392636af6eb575e31e
Chifwin <o.s.merezhko@gmail.com>
2024-07-16 18:36:32 +0600

    using instead of type defines

--- template.cpp
+++ template.cpp
@@ -3,13 +3,21 @@
 // #pragma GCC optimize("O3,unroll-loops")
 // #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
-#define ll long long
-#define ull unsigned long long
-#define ld long double
-#define vll vector<ll>
-#define pll pair<ll, ll>
-#define plll pair<pll, ll>
-#define lpll pair<ll, pll>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+template<class T, class comp=less<T>> using ordered_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, comp, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+// find_by_order(k) - iterator on k's elements
+// order_of_key(x) - number of elements, strictly less than x
+
+using ll = long long;
+using ull = unsigned long long;
+using LL = __int128_t;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll,ll>;
+
 #define FOR(i, start, stop) for(ll i = (start); i < (stop); i++)
 #define fi(n)  FOR(i, 0, n)
 #define fj(n)  FOR(j, 0, n)
@@ -23,17 +31,9 @@
 #define f first
 #define s second
 #define pb push_back
-#define rt return
-#define cn continue;
-#define br break;
-#define sz(x) ((ll)x.size())
 #define all(queries) begin(queries),end(queries)
 #define rall(queries) (queries).rbegin(),(queries).rend()
-#define YESNO(flag) cout<<(flag ? "YES" : "NO") <<'\n';
 #define dbg(...) {cerr << "\033[33m" << (#__VA_ARGS__) << ": "; dbg_vals(__VA_ARGS__); cerr << "\033[0m\n";}
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-using namespace std;
 
 template<class T> istream& operator>>(istream& in, vector<T>& x){for(T& i : x) in >> i; return in; }
 template<class T, class TT> istream& operator>>(istream& in, pair<T, TT>& x){ in >> x.f >> x.s; return in; }
@@ -42,12 +42,6 @@ template <typename Iter, typename=decltype(declval<Iter>().begin()), typename=en
 
 template<typename T> void dbg_vals(T x){ cerr<< "\033[31m" << x << "\033[0m"; }
 template<typename T, typename... Args> void dbg_vals(T x, Args... args){ dbg_vals(x); cerr << " | "; dbg_vals(args...); }
-
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-template<class T, class comp=less<T>> using ordered_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, comp, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
-// find_by_order(k) - iterator on k's elements
-// order_of_key(x) - number of elements, strictly less than x
 
 struct custom_hash { static uint64_t splitmix64(uint64_t x) {/*http://xorshift.di.unimi.it/splitmix64.c*/ x += 0x9e3779b97f4a7c15; x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9; x = (x ^ (x >> 27)) * 0x94d049bb133111eb; return x ^ (x >> 31);} size_t operator()(uint64_t x) const { static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}};
 template<class T> using ll_umap = unordered_map<long long, T, custom_hash>;
@@ -59,16 +53,18 @@ template<class T> using min_pq = priority_queue<T, vector<T>, greater<T>>;
 const ll MAXN = 2123456;
 const ll MOD = 1000'000'007;
 const ld EPS = 1e-10;
-const ll INF = (1ll<<60) + ((1<<30)-1);
+const ll INF = numeric_limits<ll>::max() >> 2;
 const ld PI = atanl(1)*4;
 
+template<typename T> inline ll sz(const T& x){ return x.size(); }
+void YESNO(bool flag) {cout<<(flag ? "YES" : "NO") <<'\n';}
 ll rll(ll r=numeric_limits<ll>::max(), ll l=0){ /*random long long*/ static mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count()); return uniform_int_distribution<ll>(l, r)(rng); }
 template<typename F> auto time_measure(F f){ auto start = chrono::steady_clock::now(); f(); auto end = chrono::steady_clock::now(); return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count(); }
 array<ll, 3> extgcd(ll a, ll b) { if (!b) return {a, 1, 0}; auto [d, y, x] = extgcd(b, a % b); return {d, x, y - a/b * x}; } // {gcd, x, y} such that a*x + b*y = gcd(a, b)
 ll gcd(ll x, ll y){ return __gcd(x, y); }
 ll binpow(ll x, ll p, ll mod){ if (p < 0) return 0; x %= mod; ll ans = 1; while(p){ if (p&1) ans = (ans*x)%mod; x = (x*x)%mod; p >>= 1; } return ans; }
 ll invmod(ll x, ll mod=MOD){ return binpow(x, mod-2, mod); }
-ll sign(ll x){ if (x < 0) rt -1; rt !!x; }
+ll sign(ll x){ return (x < 0 ? -1 : !!x); }
 void FREOPEN(string s){ freopen(string(s + ".in").c_str(), "r", stdin); freopen(string(s + ".out").c_str(), "w", stdout); }
 template<class T, class TT> bool mineq(T& a, const TT& b){ if (b < a) {a = b; return true;} return false;}
 template<class T, class TT> bool maxeq(T& a, const TT& b){ if (b > a) {a = b; return true;} return false;}
@@ -82,7 +78,7 @@ void solve(){
 }
 
 int main(){
-    IOS;
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     ll _ = 1;
     cin >> _;
     while(_--) {
