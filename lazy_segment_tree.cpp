244fb0839f5f427722be9b6a3f1c080080f88ee2
Chifwin <o.s.merezhko@gmail.com>
2024-09-23 22:21:10 +0600

    restore some int's for less memory consumption

--- lazy_segment_tree.cpp
+++ lazy_segment_tree.cpp
@@ -13,13 +13,13 @@
         Persistent:       https://codeforces.com/contest/500/submission/249040072
 */
 
-template<class Info, class Tag, ll type=0>
+template<class Info, class Tag, int type=0>
 class LazySegmentTree {
     // type == 0 -> only lazy propagation; type == 1 -> implicit; type == 2 -> persistent
-    template<ll typ, bool dummy> struct InfoTag_t{
+    template<int typ, bool dummy> struct InfoTag_t{
         Info info;
         Tag tag;
-        ll l=-1, r=-1;
+        int l=-1, r=-1;
     };
     template<bool dummy> struct InfoTag_t<0, dummy>{
         Info info;
@@ -28,30 +28,30 @@ class LazySegmentTree {
     using InfoTag = InfoTag_t<type, true>;
     ll n;
     vector<InfoTag> tree;
-    inline ll new_node(InfoTag node=InfoTag()){ tree.push_back(node); return tree.size()-1; }
-    inline ll lson(ll v, [[maybe_unused]] bool create=true){
+    inline int new_node(InfoTag node=InfoTag()){ tree.push_back(node); return tree.size()-1; }
+    inline int lson(int v, [[maybe_unused]] bool create=true){
         if constexpr(type != 0){
             if (tree[v].l == -1 && create) tree[v].l = new_node();
             return tree[v].l; 
         }
         return v << 1;
     }
-    inline ll rson(ll v, [[maybe_unused]] bool create=true){
+    inline int rson(int v, [[maybe_unused]] bool create=true){
         if constexpr(type != 0){
             if (tree[v].r == -1 && create) tree[v].r = new_node();
             return tree[v].r; 
         }
         return v << 1 | 1;
     }
-    void pull(ll id) {
-        ll l = lson(id, false), r = rson(id, false);
+    void pull(int id) {
+        int l = lson(id, false), r = rson(id, false);
         tree[id].info = (l == -1 ? Info() : tree[l].info) + (r == -1 ? Info() : tree[r].info);
     }
-    void apply(ll id, const Tag &v, ll len) {
+    void apply(int id, const Tag &v, ll len) {
         tree[id].info.merge(v, len);
         tree[id].tag += v;
     }
-    void push(ll id, ll l, ll r) {
+    void push(int id, ll l, ll r) {
         ll m = (l + r) >> 1;
         Tag t = tree[id].tag;
         if constexpr (type != 0) if (t.empty()) return;
@@ -59,8 +59,8 @@ class LazySegmentTree {
         apply(rson(id), t, r - m);
         tree[id].tag = Tag();
     }
-    ll modify(ll id, ll l, ll r, ll p, const Info &v) {
-        ll new_root = id;
+    int modify(int id, ll l, ll r, ll p, const Info &v) {
+        int new_root = id;
         if (r == l) {
             if constexpr(type == 2) new_root = new_node();
             tree[new_root].info = v;
@@ -70,24 +70,24 @@ class LazySegmentTree {
         push(id, l, r);
         if constexpr(type == 2) new_root = new_node(tree[id]);
         if (p <= m){
-            ll ne = modify(lson(id), l, m, p, v);
+            [[maybe_unused]] int ne = modify(lson(id), l, m, p, v);
             if constexpr(type == 2) tree[new_root].l = ne;
         } else {
-            ll ne = modify(rson(id), m + 1, r, p, v);
+            [[maybe_unused]] int ne = modify(rson(id), m + 1, r, p, v);
             if constexpr(type == 2) tree[new_root].r = ne;
         }
         pull(new_root);
         return new_root;
     }
-    Info rangeQuery(ll id, ll l, ll r, ll ql, ll qr) {
+    Info rangeQuery(int id, ll l, ll r, ll ql, ll qr) {
         if (qr < l || r < ql || id == -1) return Info();
         if (ql <= l && r <= qr) return tree[id].info;
         ll m = (l + r) >> 1;
         push(id, l, r);
         return rangeQuery(lson(id, false), l, m, ql, qr) + rangeQuery(rson(id, false), m + 1, r, ql, qr);
     }
-    ll rangeApply(ll id, ll l, ll r, ll ql, ll qr, const Tag &v) {
-        ll new_root = id;
+    int rangeApply(int id, ll l, ll r, ll ql, ll qr, const Tag &v) {
+        int new_root = id;
         if (qr < l || r < ql) return id;
         if constexpr(type == 2) new_root = new_node(tree[id]);
         if (ql <= l && r <= qr) {
@@ -110,7 +110,7 @@ public:
     }
     template <typename T>
     LazySegmentTree(const vector<T>& init, ll q=0) : LazySegmentTree((ll)init.size(), q) {
-        function<void(ll, ll, ll)> build = [&](ll id, ll l, ll r){
+        function<void(ll, ll, ll)> build = [&](int id, ll l, ll r){
             if (r == l) {
                 tree[id].info = init[l];
                 return;
@@ -122,13 +122,13 @@ public:
         };
         build(1, 0, n - 1);
     }
-    ll modify(ll p, const Info &v, ll ver=1) {
+    int modify(ll p, const Info &v, int ver=1) {
         return modify((type == 2 ? ver : 1), 0, n - 1, p, v);
     }
-    ll rangeApply(ll ql, ll qr, const Tag &v, ll ver=1) {
+    int rangeApply(ll ql, ll qr, const Tag &v, int ver=1) {
         return rangeApply((type == 2 ? ver : 1), 0, n - 1, ql, qr, v);
     }
-    Info rangeQuery(ll ql, ll qr, ll ver=1) {
+    Info rangeQuery(ll ql, ll qr, int ver=1) {
         return rangeQuery((type == 2 ? ver : 1), 0, n - 1, ql, qr);
     }
     /*
