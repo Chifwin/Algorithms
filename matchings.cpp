887e03ad5189271101679a4d9feabff8f60d6d8d
chifwin <o.s.merezhko@gmail.com>
2024-12-17 23:17:14 +0500

    matching simplification and example

--- matchings.cpp
+++ matchings.cpp
@@ -2,12 +2,18 @@
 
 /*
     Find maximal matching in bipartite graph.
+
+    Hint: NEVER user Kuhn
+
     Is realization of Hopcroft-Karp/Dinitz algorithm as described here:
         https://codeforces.com/blog/entry/118098
     Doesn't require division on two parts, so edges could be undirected
     In case memory is important - need only edges from left to right part and
         vertices of left part have lower indices than right.
-    https://judge.yosupo.jp/submission/201755
+
+    Example:
+        https://judge.yosupo.jp/submission/201755
+        https://codeforces.com/contest/2052/submission/297016240
 */
 
 vector<pair<int,int>> matching_bipartite(vll gr[], int n){
@@ -25,7 +31,6 @@ vector<pair<int,int>> matching_bipartite(vll gr[], int n){
     for(int i = 0; i < n; i++) if (is_l[i]) match[i] = 0;
     auto kuhn = [&](auto && self, int x) -> bool {
         for(int &i = ptr[x]; i < int(gr[x].size()); i++){
-            if (is_l[gr[x][i]]) continue;
             int &u = match[gr[x][i]];
             if (u == -1 || (dist[u] == dist[x] + 1 && self(self, u))) return (match[u = x] = true);
         }
@@ -36,8 +41,7 @@ vector<pair<int,int>> matching_bipartite(vll gr[], int n){
         int l = 0, r = 0, flag = false, x, u;
         for(int i = 0; i < n; i++) if (is_l[i] && !match[i]) dist[i] = 0, q[r++] = i;
         while(l < r){
-            if (is_l[x = q[l++]]) for(auto v : gr[x]){
-                if (is_l[v]) continue;
+            for(auto v : gr[x = q[l++]]){
                 flag |= ((u = match[v]) == -1);
                 if (u != -1 && dist[x] + 1 < dist[u]) dist[u] = dist[x]+1, q[r++] = u;
             }
@@ -47,6 +51,7 @@ vector<pair<int,int>> matching_bipartite(vll gr[], int n){
         for(int i = 0; i < n; i++) if (is_l[i] && !match[i]) kuhn(kuhn, i);
     }
     vector<pair<int, int>> ans;
+    ans.reserve(n/2);
     for(int i = 0; i < n; i++) if (!is_l[i] && match[i] != -1) ans.emplace_back(match[i], i);
     return ans;
 }
