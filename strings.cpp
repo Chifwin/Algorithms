1bcb7935f1afb3e2903c6b5c966d94c4f453e60a
chifwin <o.s.merezhko@gmail.com>
2025-06-17 19:44:20 +0500

    add suffix array

--- strings.cpp
+++ strings.cpp
@@ -12,6 +12,11 @@
 
     StringHash:
         Rolling hash for strings
+    
+    SuffixArray:
+        Computes Suffix array and Longest common prefix of suffixes in O(n*log(n))
+        Example:
+            https://vjudge.net/solution/61252382
 */
 
 class AhoCorasick{
@@ -84,3 +89,23 @@ public:
     }
     int get(){ return get(0, (int)prefs.size()-1); }
 };
+
+array<vector<int>, 2> SuffixArray(string& s) { // or basic_string<int>
+    int n = sz(s) + 1, k = 0, a, b, lim = *max_element(all(s))+1;
+    vector<int> x(all(s)), y(n), ws(max(n, lim)), sa(n), lcp(n);
+    x.push_back(0), iota(all(sa), 0);
+    for (int j = 0, p = 0; p < n; j = max(1, j * 2), lim = p) {
+        p = j, iota(all(y), n - j);
+        fi(n) if (sa[i] >= j) y[p++] = sa[i] - j;
+        fill(all(ws), 0);
+        fi(n) ws[x[i]]++;
+        fio(lim) ws[i] += ws[i - 1];
+        for (int i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
+        swap(x, y), p = 1, x[sa[0]] = 0;
+        fio(n) a = sa[i - 1], b = sa[i], x[b] = (y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
+    }
+    for (int i = 0, j; i < n - 1; lcp[x[i++]] = k)
+        for (k && k--, j = sa[x[i] - 1]; s[i + k] == s[j + k]; k++);
+    sa.erase(sa.begin()); lcp.erase(lcp.begin());
+    return {sa, lcp};
+}
